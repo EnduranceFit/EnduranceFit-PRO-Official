9e2c0c1e59aa350472318ae4d7d37be5eec015c5
EnduranceFit <luthierjonatanalves@gmail.com>
2026-03-17 00:09:22 -0300

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import type { NextConfig } from "next";
+
+const nextConfig = {
+    output: 'export',
+        reactStrictMode: false,
+        serverExternalPackages: ['archiver'],
+        images: {
+    remotePatterns: [
+{
+        protocol: "https",
+                  hostname: "picsum.photos",
+                  port: "",
+                  pathname: "/**",
+          },
+          ],
+      },
+      };
+
+export default nextConfig;
